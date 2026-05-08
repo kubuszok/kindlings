2ee6056978e1a1bc91e3382ea341833cbcb4cf98
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2026-05-09 00:35:09 +0200

    Add real avro4s 5.x benchmarks on Scala 3
    Use avro4s 5.x Encoder/Decoder/SchemaFor directly via Avro4sFormat
    wrapper (avro4s 5.x RecordFormat requires ToRecord/FromRecord which
    have a different derivation path). Both auto and semi-auto instances
    now use actual avro4s derivation on Scala 3.

--- benchmarks/src/main/scala-3/hearth/kindlings/benchmarks/OriginalAvroSemiAutoInstances.scala
+++ benchmarks/src/main/scala-3/hearth/kindlings/benchmarks/OriginalAvroSemiAutoInstances.scala
@@ -1,9 +1,22 @@
 package hearth.kindlings.benchmarks
 
-import org.apache.avro.generic.GenericRecord
+import com.sksamuel.avro4s.{Decoder as AvroDecoder, Encoder as AvroEncoder, SchemaFor}
 
-// avro4s 5.x Scala 3 semi-auto has incompatible API; delegate to auto instances
 object OriginalAvroSemiAutoInstances {
-  val simpleCCFormat: Avro4sCompat[SimpleCC] = OriginalAvroAutoInstances.simpleCCFormat
-  val personFormat: Avro4sCompat[Person] = OriginalAvroAutoInstances.personFormat
+  given SchemaFor[Address] = SchemaFor.derived
+  given avroEncoderAddress: AvroEncoder[Address] = AvroEncoder.derived
+  given avroDecoderAddress: AvroDecoder[Address] = AvroDecoder.derived
+
+  given SchemaFor[SimpleCC] = SchemaFor.derived
+  given avroEncoderSimpleCC: AvroEncoder[SimpleCC] = AvroEncoder.derived
+  given avroDecoderSimpleCC: AvroDecoder[SimpleCC] = AvroDecoder.derived
+
+  given SchemaFor[Person] = SchemaFor.derived
+  given avroEncoderPerson: AvroEncoder[Person] = AvroEncoder.derived
+  given avroDecoderPerson: AvroDecoder[Person] = AvroDecoder.derived
+
+  val simpleCCFormat: Avro4sFormat[SimpleCC] =
+    new Avro4sFormat(summon[SchemaFor[SimpleCC]].schema, avroEncoderSimpleCC, avroDecoderSimpleCC)
+  val personFormat: Avro4sFormat[Person] =
+    new Avro4sFormat(summon[SchemaFor[Person]].schema, avroEncoderPerson, avroDecoderPerson)
 }
