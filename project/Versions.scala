c2e1c5f839f962e7d7fe889da504d83c27d3389a
Scala Steward <scala_steward@virtuslab.com>
2026-06-24 18:03:31 +0000

    Update circe-core, circe-generic, ... to 0.14.16

--- project/Versions.scala
+++ project/Versions.scala
@@ -25,7 +25,7 @@ object versions {
   val avro4s213 = "4.1.2"
   val avro4s3 = "5.0.15"
   val cats = "2.13.0"
-  val circe = "0.14.15"
+  val circe = "0.14.16"
   val iron = "3.3.1"
   val jsoniterScala = "2.38.16"
   val kittens = "3.5.0"
